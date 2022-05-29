dede604db55420065e6acf5ec2c229a3502eb933
pahirdinhamut <69786935+pahirdinhamut@users.noreply.github.com>
2022-05-29 22:23:44 +0200

    Hotel CURD is ok :D

--- router/hotels.js
+++ router/hotels.js
@@ -18,16 +18,44 @@ router.post("/", async (req, res) => {
 //update
 router.put("/:id", async (req, res) => {
   try {
-    const updatedHotel = await Hotels.findByIdAndUpdate(req.params.id, {
-      $set: req.body
-    });
+    const updatedHotel = await Hotels.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body
+      },
+      { new: true } // yenilemesi icin
+    );
     res.status(200).json(updatedHotel);
   } catch (error) {
     res.status(500).json(error);
   }
 });
 //delete
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotels.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel has been delete !!");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //get
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotels.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 //get all
+router.get("/", async (req, res) => {
+  try {
+    const hotels = await Hotels.find();
+    res.status(200).json(hotels);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 export default router;
